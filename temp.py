a58c77b3b3c64bf7fbff217ec06f44315d8faf0e
david <davidguo123456@gmail.com>
2024-04-11 12:51:11 -0400

    Removed unnessecary 'temp.py' file

--- temp.py
+++
@@ -1,2 +0,0 @@
-import ECE324.generate_embeddings as generate_embeddings
-generate_embeddings.encode_ns('~/ECE324/lmd_wan/train/ns_original.tfrecord', '~/ECE324/lmd_wan/train/encoded_original.tfrecord')
